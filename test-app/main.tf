43b7462b142cc7f3154c44b18193051d48af6290
Andrew Kemp-Dahlberg <Andrew.KempDahlberg@UN-MAC-Andrew.KempDahlberg>
2025-03-02 01:58:09 -0600

    added example app

---
+++ test-app/main.tf
@@ -0,0 +1,34 @@
+module app {
+    source = "https://github.com/andrew-kemp-dahlberg/terraform-okta-app" #This is the module this always stays the same.
+    label = "Example" #This is the name of the app displayed to users
+    admin_note = {
+        saas_mgmt_name = "example" #This is the name in Torii
+        sso_enforced = false #On initial provision this should be set to false. When you are sure you have enforced SSO change to true.
+        lifecycle_automations = { 
+            provisioning = {  #This is how users are provisioned to the app
+                type = "SCIM" #this can equal "SCIM", "ADP", "Okta Workflows fully automated", "Okta workflows Zendesk", "AWS", "None"
+                link = "https://automation.com" #this is the link to where the automation is provisioned this can just be the app url
+                }
+            user_updates = { #This is how updates to users are pushed to the app ie. email change
+                type = "SCIM"
+                link = "https://automation.com"
+            } 
+            deprovisioning = { #this is how users are deprovisioned from the app
+                type = "SCIM"
+                link = "https://automation.com" 
+            } 
+        } 
+        service_accounts = ["service.account@company.com", "service.account2@company.com"] #This is a list of service accounts
+        app_owner = "app.owner@company.com" #this is who owns the app 
+        last_access_audit_date = "string" #this is when access to the app was last audited. This should be done upon creation
+        additional_notes = <<EOT
+        These are additional notes that you can add. 
+        You can add multiple lines etc. 
+        A portion of these notes are uploaded directly to the app. 
+        All of the fields you add here are added to confluence. 
+        EOT
+    }
+    sso_url = "https://example.com"#this is the sso url 
+    audience_uri = "https://example.com" #This is audience_uri/entity id
+}
+

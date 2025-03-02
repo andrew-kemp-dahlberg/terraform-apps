7ddc82a89e9d5e2a52414da060ac1b29f1a7aeb6
Andrew Kemp-Dahlberg <Andrew.KempDahlberg@UN-MAC-Andrew.KempDahlberg>
2025-03-02 17:18:57 -0600

    added accounting_name variable to admin notes

--- test-app/main.tf
+++ test-app/main.tf
@@ -2,25 +2,26 @@ module app {
     source = "https://github.com/andrew-kemp-dahlberg/terraform-okta-app" #This is the module this always stays the same.
     label = "Example" #This is the name of the app displayed to users
     admin_note = {
-        saas_mgmt_name = "example" #This is the name in Torii
+        saas_mgmt_name = "example" #This is the name of the app in Torii
+        accounting_name = "Example" #This is the name of the app in Netsuite
         sso_enforced = false #On initial provision this should be set to false. When you are sure you have enforced SSO change to true.
         lifecycle_automations = { 
             provisioning = {  #This is how users are provisioned to the app
                 type = "SCIM" #this can equal "SCIM", "ADP", "Okta Workflows fully automated", "Okta workflows Zendesk", "AWS", "None"
-                link = "https://automation.com" #this is the link to where the automation is provisioned this can just be the app url
+                link = "" #this is the link to where the automation is provisioned this can just be the app url or left empty for SCIM or None. 
                 }
             user_updates = { #This is how updates to users are pushed to the app ie. email change
                 type = "SCIM"
-                link = "https://automation.com"
+                link = ""
             } 
             deprovisioning = { #this is how users are deprovisioned from the app
-                type = "SCIM"
+                type = "Okta Workflows Fully Automated"
                 link = "https://automation.com" 
             } 
         } 
         service_accounts = ["service.account@company.com", "service.account2@company.com"] #This is a list of service accounts
         app_owner = "app.owner@company.com" #this is who owns the app 
-        last_access_audit_date = "string" #this is when access to the app was last audited. This should be done upon creation
+        last_access_audit_date = "string" #this is when access to the app was last audited. This should be the date of creation since access needs to be audited before provisioning.
         additional_notes = <<EOT
         These are additional notes that you can add. 
         You can add multiple lines etc. 
